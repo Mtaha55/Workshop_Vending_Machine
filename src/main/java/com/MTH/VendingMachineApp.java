8d587e646027c071946370eeb26c1ab87fe3be60
Mtaha55 <38652177+Mtaha55@users.noreply.github.com>
2024-03-26 12:02:17 +0100

    Vending Machine with Snack and Candy

---
+++ src/main/java/com/MTH/VendingMachineApp.java
@@ -0,0 +1,23 @@
+package com.MTH;
+import com.MTH.Model.Candy;
+import com.MTH.Model.Snack;
+
+
+public class VendingMachineApp {
+    public static void main(String[] args) {
+        // Creating instances of different products
+        Candy candy = new Candy(1, 1.5, "Chocolate Bar", "Milk Chocolate");
+        Snack snack = new Snack(2, 2.0, "Potato Chips", 150);
+
+
+        // Examining products
+        System.out.println(candy.examine());
+        System.out.println(snack.examine());
+
+
+        // Using products
+        System.out.println(candy.use());
+        System.out.println(snack.use());
+
+    }
+}
